c49928e2fdfac611efbbcd23f6fc535b91ea01c4
Lola García <mariadolores.garcia@bosonit.com>
2023-08-28 17:22:39 +0200

    add firebase data

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -12,19 +12,36 @@ export class InfoPageService {
   info:InfoPage = {}; //creamos interface del objeto con las propiedades del JSON
   isLoaded: boolean = false;
 
+  team: any[] = [];
+
   constructor( private http: HttpClient) {
 
-    //console.log('Info Page Service ready');
+      this.loadInfo();
+      this.loadTeam();
+  }
 
-    //Leer el archivo JSON creado con la info 
+  private loadInfo() {
+    //Leer el archivo JSON 
     this.http.get('assets/data/data-page.json')
-      .subscribe( (resp: InfoPage) => {
-        
-        this.isLoaded = true;
-        this.info = resp;
-        console.log(resp);
-        
-      })
+    .subscribe( (resp: InfoPage) => {
+      
+      this.isLoaded = true;
+      this.info = resp;
+      //console.log(resp);
+  
+    })
+  }
 
+  private loadTeam() {
+      //Leer el archivo de Firebase
+      this.http.get('https://html-angular-a0d50-default-rtdb.europe-west1.firebasedatabase.app/team.json')
+      .subscribe( (resp: any) => {
+        
+        this.team = resp;
+        //console.log(resp);
+
+      });
+
+      // this.team = resp;
   }
 }
