586460267b24be4dce086072dead387c202f2d97
Lola García <mariadolores.garcia@bosonit.com>
2023-08-28 12:24:51 +0200

    add README basic details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const routes: Routes = [
+  {path: '', component:PortfolioComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo:''}, //redirecciona a PortFolio
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
